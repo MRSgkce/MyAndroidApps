9956bfb471ca2b39efe8e7fca47e0de8001df4df
murside <mursidegokce985@gmail.com>
2024-07-08 12:04:26 +0300

    birinci sayfadan ikinci sayfaya geçiş fonksiyounları ile onviewcreated eklendi.

--- MyNavigationApp/app/src/main/java/com/murside/mynavigationapp/BirinciFragment.kt
+++ MyNavigationApp/app/src/main/java/com/murside/mynavigationapp/BirinciFragment.kt
@@ -1,37 +1,52 @@
 package com.murside.mynavigationapp
 
 import android.os.Bundle
+import android.text.Layout.Directions
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.murside.mynavigationapp.databinding.FragmentBirinciBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BirinciFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BirinciFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+    private var _binding : FragmentBirinciBinding?= null
+    private  val binding get( )= _binding!!
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_birinci, container, false)
+    _binding=FragmentBirinciBinding.inflate(inflater,container,false)
+    val view=binding.root
+        return view
+    }
+    fun sonraki(view:View){
+        val isim=binding.editText.text.toString()
+    val action=BirinciFragmentDirections.actionBirinciFragmentToIkinciFragment(isim)
+        Navigation.findNavController(view).navigate(action)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.editText.setText(" ")
+        binding.button.setOnClickListener{
+            sonraki(it)
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
     }
 
 
--- MyNavigationApp/app/src/main/java/com/murside/mynavigationapp/ikinciFragment.kt
+++ MyNavigationApp/app/src/main/java/com/murside/mynavigationapp/ikinciFragment.kt
@@ -5,18 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.murside.mynavigationapp.databinding.FragmentBirinciBinding
+import com.murside.mynavigationapp.databinding.FragmentIkinciBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ikinciFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ikinciFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+    private var _binding : FragmentIkinciBinding?= null
+    private  val binding get( )= _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +22,17 @@ class ikinciFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ikinci, container, false)
+        _binding=FragmentIkinciBinding.inflate(inflater,container,false)
+        val view=binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+           val isim= ikinciFragmentArgs.fromBundle(it).kullaiciismi
+            binding.textView.text=isim
+        }
     }
 
 }
